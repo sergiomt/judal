babbf96963b537d2b6695b3e328165226f2cd1de
Sergio Montoro <sergiom@knowgate.com>
2017-02-12 16:42:07 +0000

    Add support for Long, Float, Double and Date arrays

--- scala-adaptor/src/main/scala/org/judal/storage/scala/postgresql/PostgreSQLFieldHelper.scala
+++ scala-adaptor/src/main/scala/org/judal/storage/scala/postgresql/PostgreSQLFieldHelper.scala
@@ -1,7 +1,10 @@
 package org.judal.storage.scala.postgresql
 
 import java.lang.Iterable
+import java.util.Date
 import java.lang.reflect.Constructor
+
+import java.sql.Timestamp
 import java.sql.SQLException
 
 import scala.collection.mutable.Map
@@ -17,30 +20,97 @@ class PostgreSQLFieldHelper extends FieldHelper {
  
   private var cnr: Constructor[Iterable[java.util.Map.Entry[String, String]]] = null
 
-	def getIntegerArray(rec: Record, key: String) : Array[Integer] = {
-		val arr = rec.apply(key).asInstanceOf[java.sql.Array]
-		if (null!=arr) {
-			try {
-			  arr.getArray().asInstanceOf[Array[java.lang.Integer]]
-			} catch  {
-			  case sqle: SQLException => throw new ClassCastException(sqle.getMessage())
+	private def getArray[T](obj: AnyRef) : Array[T] = {
+		if (null!=obj) {
+		  if (obj.isInstanceOf[java.sql.Array]) {
+		    val arr = obj.asInstanceOf[java.sql.Array]	  
+			  try {
+			    arr.getArray().asInstanceOf[Array[T]]
+			  } catch  {
+			    case sqle: SQLException => throw new ClassCastException(sqle.getMessage())
+			  }
+		  } else {
+		    obj.asInstanceOf[Array[T]]
+		  }
+		} else {
+			null
+		}		  
+	}
+
+  def getIntegerArray(rec: Record, key: String) : Array[java.lang.Integer] = {
+		val obj = rec.apply(key)
+    if (obj.isInstanceOf[Array[Int]])
+      obj.asInstanceOf[Array[Int]] map java.lang.Integer.valueOf
+    else
+		  getArray[Integer](obj)
+  }
+
+  def getLongArray(rec: Record, key: String) : Array[java.lang.Long] = {
+		val obj = rec.apply(key)
+    if (obj.isInstanceOf[Array[Long]])
+      obj.asInstanceOf[Array[Long]] map java.lang.Long.valueOf
+    else
+		  getArray[java.lang.Long](obj)
+  }
+
+  def getFloatArray(rec: Record, key: String) : Array[java.lang.Float] = {
+		val obj = rec.apply(key)
+    if (obj.isInstanceOf[Array[Float]])
+      obj.asInstanceOf[Array[Float]] map java.lang.Float.valueOf
+    else
+		  getArray[java.lang.Float](obj)
+  }
+
+  def getDoubleArray(rec: Record, key: String) : Array[java.lang.Double] = {
+		val obj = rec.apply(key)
+    if (obj.isInstanceOf[Array[Double]])
+      obj.asInstanceOf[Array[Double]] map java.lang.Double.valueOf
+    else
+		  getArray[java.lang.Double](obj)
+  }
+  
+	def getStringArray(rec: Record, key: String) : Array[String] = {
+		val obj = rec.apply(key)
+		if (null!=obj) {
+		  if (obj.isInstanceOf[java.sql.Array]) {
+		    val arr = obj.asInstanceOf[java.sql.Array]	  
+			  try {
+				  arr.getArray().asInstanceOf[Array[String]]
+			  } catch {
+			    case sqle: SQLException => throw new ClassCastException(sqle.getMessage())
+			  }
+		  } else {
+			    obj.asInstanceOf[Array[String]]
 			}
 		} else {
 			null
 		}		  
 	}
 
-	def getStringArray(rec: Record, key: String) : Array[String] = {
-		val arr = rec.apply(key).asInstanceOf[java.sql.Array]
-		if (null!=arr) {
-			try {
-				arr.getArray().asInstanceOf[Array[String]]
-			} catch {
-			  case sqle: SQLException => throw new ClassCastException(sqle.getMessage())
-			}
+		def getDateArray(rec: Record, key: String) : Array[Date] = {
+		val obj = rec.apply(key);
+		if (null==obj) {
+		  null
+		} else if (obj.isInstanceOf[Array[Date]]) {
+			obj.asInstanceOf[Array[Date]]
+		} else if (obj.isInstanceOf[java.sql.Array]) {		     
+			  try {
+				  val arr = obj.asInstanceOf[java.sql.Array].getArray()
+				  if (arr.isInstanceOf[Array[Date]]) {
+				    arr.asInstanceOf[Array[Date]]
+				  } else if (arr.isInstanceOf[Array[Timestamp]]) {
+            arr.asInstanceOf[Array[Timestamp]] map (t => new Date(t.getTime()))
+				  } else if (arr.isInstanceOf[Array[java.sql.Date]]) {
+            arr.asInstanceOf[Array[java.sql.Date]] map (d => new Date(d.getYear(), d.getMonth(), d.getDate()))
+				  } else {
+		        throw new ClassCastException("PostgreSQLFieldHelper.getDateArray() cannot cast java.sql.Array to Array[Date]");
+				  }
+			  } catch {
+			    case sqle: SQLException => throw new ClassCastException(sqle.getMessage())
+			  }
 		} else {
-			null
-		}		  
+		  throw new ClassCastException("PostgreSQLFieldHelper.getDateArray() cannot cast from "+obj.getClass().getName()+" to Array[Date]");
+		}
 	}
 
 	def getLatLong(rec: Record, key: String ) : LatLong = {
